431a50adbc97f766e3b7519b0b76b2aea2fb406f
cscherbert1 <cscherbert89@gmail.com>
2017-02-26 14:44:03 -0600

    Added some documentation/ TODO comments

--- src/dip/lab3/student/solution1/Startup.java
+++ src/dip/lab3/student/solution1/Startup.java
@@ -4,6 +4,9 @@ package dip.lab3.student.solution1;
  *To demonstrate polymorphism, experiment with the different ways to input and output a message using comments. 
  * You can only have one input and one output uncommented at a time.
  * 
+ * 
+ * FIND REASON WHY KEYBOARD INPUT AND GUI OUTPUT DO NOT WORK TOGETHER!
+ * 
  * @author cscherbert1
  */
 public class Startup {
